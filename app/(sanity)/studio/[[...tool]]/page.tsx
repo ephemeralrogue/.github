f302d8eaa2e5e9a575353e68c0222f6566e06aca
effem <ephemeralrogue@proton.me>
2024-04-08 18:17:23 -0700

    working with static site generator funciton for build step

--- app/(sanity)/studio/[[...tool]]/page.tsx
+++ app/(sanity)/studio/[[...tool]]/page.tsx
@@ -7,3 +7,8 @@ export const dynamic = "force-static";
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
+
+export function generateStaticParams() {
+  const pages = ['Presentation', 'Vision', 'Structure'];
+  return pages.map((page) => ({ name: page }));
+}
