ba56677fdb927382ec70ca0b53e7bd57ba98960d
effem <ephemeralrogue@proton.me>
2024-04-08 18:44:48 -0700

    work around

--- app/(sanity)/studio/[[...tool]]/page.tsx
+++ app/(sanity)/studio/[[...tool]]/page.tsx
@@ -1,5 +1,6 @@
 import { NextStudio } from "next-sanity/studio";
-
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { groq } from "next-sanity";
 import config from "@/sanity.config";
 
 export const dynamic = "force-static";
@@ -8,7 +9,17 @@ export default function StudioPage() {
   return <NextStudio config={config} />;
 }
 
-export function generateStaticParams() {
-  const pages = ['draft'];
+export async function generateStaticParams() {
+  const pages = ['presentation', 'structure', 'vision'];
   return pages.map((page) => ({ name: page }));
 }
+
+/*
+export async function generateStaticParams() {
+  return sanityFetch<{ tool: string }[]>({
+    query: groq`*[_type == "post" && defined(slug.current)]{"slug": slug.current}`,
+    perspective: "published",
+    stega: false,
+  });
+}
+*/
