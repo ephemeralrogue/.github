2aa06d80bfb817283a8a1f21bef6495a29ad97d1
effem <ephemeralrogue@proton.me>
2024-04-08 19:14:59 -0700

    vercel deployment push

--- app/(sanity)/studio/[[...tool]]/page.tsx
+++ app/(sanity)/studio/[[...tool]]/page.tsx
@@ -8,18 +8,3 @@ export const dynamic = "force-static";
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
-
-export async function generateStaticParams() {
-  const pages = ['presentation', 'structure', 'vision'];
-  return pages.map((page) => ({ name: page }));
-}
-
-/*
-export async function generateStaticParams() {
-  return sanityFetch<{ tool: string }[]>({
-    query: groq`*[_type == "post" && defined(slug.current)]{"slug": slug.current}`,
-    perspective: "published",
-    stega: false,
-  });
-}
-*/
